9675ea6179b44deb92a793b007fd5a65ea39424f
Tsandesh <uchihazoro02@gmail.com>
2023-06-03 23:09:12 +0545

    fix: button UI

--- packages/app/src/components/resuables/button/Button.jsx
+++ packages/app/src/components/resuables/button/Button.jsx
@@ -3,12 +3,14 @@ import React from "react";
 
 const ButtonComponent = ({ onPress, label, children }) => {
   return (
-    <View className="bg-primary rounded-full mb-4">
+    <View className="bg-primary rounded-full mb-2 w-60">
       <TouchableOpacity
-        className="px-8 py-2  flex-row bg-slate-500 rounded-full font-bold mb-2 items-center"
+        className="px-8 py-2 flex-row text-center mb-2 justify-center items-center"
         onPress={onPress}
       >
-        <Text className="text-[#F2F2F2] mx-4 font-bold">{label}</Text>
+        <Text className="text-[#F2F2F2] mx-4 text-[14px] leading-5 font-[500]">
+          {label}
+        </Text>
         {children}
       </TouchableOpacity>
     </View>
